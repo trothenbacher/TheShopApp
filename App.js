aafa19a118974790dce050cc9b1008672157282c
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-08-28 22:43:32 +0200

    add navigator

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer
@@ -12,7 +13,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-
+      <ShopNavigator />
     </Provider>
   );
 }
