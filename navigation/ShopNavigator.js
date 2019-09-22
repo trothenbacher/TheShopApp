e698a20edd92dfdd1d327b46a414b59fc43a520f
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-22 10:10:00 +0200

    navigation to edit screen / minor fixes

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -8,6 +8,7 @@ import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
+import EditProductScreen from '../screens/user/EditProductScreen';
 import Colors from '../constants/Colors';
 
 const defaultNavOptions = {
@@ -54,7 +55,8 @@ const OrdersNavigator = createStackNavigator({
 });
 
 const AdminNavigator = createStackNavigator({
-    UserProducts: UserProductsScreen
+    UserProducts: UserProductsScreen,
+    EditProduct: EditProductScreen
 }, {
     navigationOptions: {
         drawerIcon: drawerConfig =>
--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -12,6 +12,11 @@ import * as productsActions from '../../store/actions/products';
 const UserProductsScreen = props => {
     const userProducts = useSelector(state => state.products.userProducts);
     const dispatch = useDispatch();
+
+    const editProductHandler = (id) => {
+        props.navigation.navigate('EditProduct', { productId: id });
+    };
+
     return (
         <FlatList
             data={userProducts}
@@ -21,10 +26,12 @@ const UserProductsScreen = props => {
                     image={itemData.item.imageUrl}
                     title={itemData.item.title}
                     price={itemData.item.price}
-                    onSelect={() => { }}
+                    onSelect={() => {
+                        editProductHandler(itemData.item.id);
+                    }}
                 >
                     <Button color={Colors.primary} title="Edit" onPress={() => {
-
+                        editProductHandler(itemData.item.id);
                     }} />
                     <Button color={Colors.primary} title="Delete" onPress={() => {
                         dispatch(productsActions.deleteProduct(itemData.item.id));
@@ -43,6 +50,14 @@ UserProductsScreen.navigationOptions = navData => {
                 <Item title='Menu' iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
                     onPress={() => {
                         navData.navigation.toggleDrawer();
+                    }} />
+            </HeaderButtons>
+        ),
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title='Add' iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+                    onPress={() => {
+                        navData.navigation.navigate('EditProduct');
                     }} />
             </HeaderButtons>
         )
