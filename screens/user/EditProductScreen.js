f3dadde5fdbfa2e5c9ca3b7e6ce050d1e074a049
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-22 11:51:30 +0200

    add basic user input

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,16 +1,113 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, ScrollView, Text, TextInput, Platform, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
+
+import HeaderButton from '../../components/UI/HeaderButton';
+
 
 const EditProductScreen = props => {
+    const prodId = props.navigation.getParam('productId');
+    const editedProduct = useSelector(state =>
+        state.products.userProducts.find(prod => prod.id === prodId)
+    );
+
+    const [title, setTitle] = useState(
+        editedProduct ? editedProduct.title : ''
+    );
+    const [imageUrl, setImageUrl] = useState(
+        editedProduct ? editedProduct.imageUrl : ''
+    );
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState(
+        editedProduct ? editedProduct.description : ''
+    );
+
+    const submitHandler = useCallback(() => {
+        console.log('Submitting');
+    }, []);
+
+    useEffect(() => {
+        props.navigation.setParams({ submit: submitHandler });
+    }, [submitHandler]);
+
     return (
-        <View>
-            <Text>The Edit Product Screen!</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Title</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={title}
+                        onChangeText={text => { setTitle(text) }}
+                    />
+                </View>
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Image URL</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={imageUrl}
+                        onChangeText={text => { setImageUrl(text) }}
+                    />
+                </View>
+                {editedProduct ? null : (
+                    <View style={styles.formControl}>
+                        <Text style={styles.label}>Price</Text>
+                        <TextInput
+                            style={styles.input}
+                            value={price}
+                            onChangeText={text => { setPrice(text) }}
+                        />
+                    </View>
+                )}
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Description</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={description}
+                        onChangeText={text => { setDescription(text) }}
+                    />
+                </View>
+            </View>
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-
+    form: {
+        margin: 20
+    },
+    formControl: {
+        width: '100%'
+    },
+    label: {
+        fontFamily: 'open-sans-bold',
+        marginVertical: 8
+    },
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 5,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    }
 });
+
+EditProductScreen.navigationOptions = navData => {
+    const submitFn = navData.navigation.getParam('submit');
+    return {
+        headerTitle: navData.navigation.getParam('productId')
+            ? 'Edit Product'
+            : 'Add Product',
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title='Save'
+                    iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
+                    onPress={submitFn}
+                />
+            </HeaderButtons>
+        )
+    };
+};
 
 export default EditProductScreen;
