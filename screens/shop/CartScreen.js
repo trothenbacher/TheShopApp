6dcaa502bca0aaf37b7125792bf2aa82f265d288
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-23 21:44:41 +0200

    refactoring

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -3,6 +3,7 @@ import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import Card from '../../components/UI/Card';
 import * as cartActions from '../../store/actions/cart';
 import * as ordersAction from '../../store/actions/orders';
 
@@ -29,7 +30,7 @@ const CartScreen = props => {
 
     return (
         <View style={styles.screen}>
-            <View style={styles.summary}>
+            <Card style={styles.summary}>
                 <Text style={styles.summaryText}>
                     Total:{' '}
                     <Text style={styles.amount}>
@@ -44,7 +45,7 @@ const CartScreen = props => {
                         dispatch(ordersAction.addOrder(cartItems, cartTotalAmount));
                     }}
                 />
-            </View>
+            </Card>
             <FlatList data={cartItems} keyExtractor={item => item.productId}
                 renderItem={itemData => <CartItem
                     quantity={itemData.item.quantity}
@@ -73,14 +74,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-between',
         marginBottom: 20,
-        padding: 10,
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        elevation: 5,
-        borderRadius: 10,
-        backgroundColor: 'white'
+        padding: 10
     },
     summaryText: {
         fontFamily: 'open-sans-bold',
