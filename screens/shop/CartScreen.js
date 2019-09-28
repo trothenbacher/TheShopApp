f9137fd3863ed6c7e5f2afa77fafe90cca018c55
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-28 22:01:33 +0200

    add spinner to order now

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, Button, StyleSheet, ActivityIndicator } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
@@ -8,6 +8,7 @@ import * as cartActions from '../../store/actions/cart';
 import * as ordersAction from '../../store/actions/orders';
 
 const CartScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
 
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
     const cartItems = useSelector(state => {
@@ -28,6 +29,12 @@ const CartScreen = props => {
 
     const dispatch = useDispatch();
 
+    const sendOrderHandler = async () => {
+        setIsLoading(true);
+        await dispatch(ordersAction.addOrder(cartItems, cartTotalAmount));
+        setIsLoading(false);
+    };
+
     return (
         <View style={styles.screen}>
             <Card style={styles.summary}>
@@ -37,14 +44,18 @@ const CartScreen = props => {
                         ${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}
                     </Text>
                 </Text>
-                <Button
-                    color={Colors.accent}
-                    title='Order Now'
-                    disabled={cartItems.length === 0}
-                    onPress={() => {
-                        dispatch(ordersAction.addOrder(cartItems, cartTotalAmount));
-                    }}
-                />
+                {isLoading ? (
+                    <ActivityIndicator size='small' color={Colors.primary} />
+                ) : (
+                        <Button
+                            color={Colors.accent}
+                            title='Order Now'
+                            disabled={cartItems.length === 0}
+                            onPress={sendOrderHandler}
+                        />
+                    )
+                }
+
             </Card>
             <FlatList data={cartItems} keyExtractor={item => item.productId}
                 renderItem={itemData => <CartItem
