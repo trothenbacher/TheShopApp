42cb07d789139b87eced62429d26222a2ef979bf
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-28 20:21:28 +0200

    update and delete products in firebase

--- store/actions/products.js
+++ store/actions/products.js
@@ -38,7 +38,14 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-    return { type: DELETE_PRODUCT, pid: productId };
+    return async dispatch => {
+        await fetch(`https://rn-the-shop-app-ecfb0.firebaseio.com/products/${productId}.json`,
+            {
+                method: 'DELETE',
+            }
+        );
+        dispatch({ type: DELETE_PRODUCT, pid: productId });
+    };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -75,13 +82,28 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-    return {
-        type: UPDATE_PRODUCT,
-        pid: id,
-        productData: {
-            title,
-            description,
-            imageUrl
-        }
+    return async dispatch => {
+
+        await fetch(`https://rn-the-shop-app-ecfb0.firebaseio.com/products/${id}.json`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl
+            })
+        });
+
+        dispatch({
+            type: UPDATE_PRODUCT,
+            pid: id,
+            productData: {
+                title,
+                description,
+                imageUrl
+            }
+        });
     };
 };
