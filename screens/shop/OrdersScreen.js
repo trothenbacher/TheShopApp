3b1700467460b1b143fb7e39e2b11e255afe832b
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-28 22:37:25 +0200

    add spinners to orders

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,19 +1,33 @@
-import React, { useEffect } from 'react';
-import { Text, FlatList, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, FlatList, Platform, ActivityIndicator, StyleSheet } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import OrderItem from '../../components/shop/OrderItem';
 import * as ordersActions from '../../store/actions/orders';
+import Colors from '../../constants/Colors';
 
 const OrdersScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
     const orders = useSelector(state => state.orders.orders);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(ordersActions.fetchOrders());
+        setIsLoading(true);
+        dispatch(ordersActions.fetchOrders()).then(() => {
+            setIsLoading(false);
+        });
+
     }, [dispatch]);
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size='large' color={Colors.primary} />
+            </View>
+        );
+    }
 
     return (
         <FlatList
@@ -44,5 +58,13 @@ OrdersScreen.navigationOptions = navData => {
         )
     };
 };
+
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
 
 export default OrdersScreen;
