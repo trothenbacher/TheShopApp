fe8c0c902b810273a46cbb810811c301f599cc96
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-28 16:32:14 +0200

    add middleware redux-thunk

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
+import ReduxThunk from 'redux-thunk';
+
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
 import ordersReducer from './store/reducers/orders';
 import ShopNavigator from './navigation/ShopNavigator';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -15,7 +16,7 @@ const rootReducer = combineReducers({
   orders: ordersReducer
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchFonts = () => {
   return Font.loadAsync({
