75526dd4f2435c6f9c975aed939019398ab4376c
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-28 22:22:22 +0200

    fetch orders from firebase

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, FlatList, Platform } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import OrderItem from '../../components/shop/OrderItem';
+import * as ordersActions from '../../store/actions/orders';
 
 const OrdersScreen = props => {
     const orders = useSelector(state => state.orders.orders);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(ordersActions.fetchOrders());
+    }, [dispatch]);
+
     return (
         <FlatList
             data={orders}
