45f44dd2691913e97e7a75fe5e7fdb62f3e3b996
Thomas Rothenbacher <thomas.rothenbacher@gmail.com>
2019-09-29 11:25:32 +0200

    add basic login screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { createStackNavigator, createDrawerNavigator, createAppContainer } from 'react-navigation';
+import {
+    createSwitchNavigator,
+    createStackNavigator,
+    createDrawerNavigator,
+    createAppContainer
+} from 'react-navigation';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -9,6 +14,7 @@ import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import EditProductScreen from '../screens/user/EditProductScreen';
+import AuthScreen from '../screens/user/AuthScreen';
 import Colors from '../constants/Colors';
 
 const defaultNavOptions = {
@@ -79,4 +85,15 @@ const ShopNavigator = createDrawerNavigator({
     }
 });
 
-export default createAppContainer(ShopNavigator);
+const AuthNavigator = createStackNavigator({
+    Auth: AuthScreen
+}, {
+    defaultNavigationOptions: defaultNavOptions
+});
+
+const MainNavigator = createSwitchNavigator({
+    Auth: AuthNavigator,
+    Shop: ShopNavigator
+});
+
+export default createAppContainer(MainNavigator);
